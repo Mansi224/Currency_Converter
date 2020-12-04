21fd90301005b5500c5c7fe48a7112744ebc0f40
Mansi Ladkar <ladkarmansi@gmail.com>
2020-12-04 17:27:37 +0530

    Currency Converter (Dollars to Rupees)

---
+++ app/src/main/java/com/CurrencyConverter/application/MainActivity.java
@@ -0,0 +1,67 @@
+package com.CurrencyConverter.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void btnClick(View view){
+        EditText dollarAmount = (EditText) findViewById(R.id.dollarAmount);
+        String dollars = dollarAmount.getText().toString();
+        Double doubleDollars = Double.parseDouble(dollars);
+        Double doubleInr = 73.72 * doubleDollars;
+        String toastText = "= " + doubleInr.toString() + " $";
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/CurrencyConverter/application/View.java
@@ -0,0 +1,10 @@
+package com.CurrencyConverter.application;
+
+public class View {
+    public class OnClickListener implements android.view.View.OnClickListener {
+        @Override
+        public void onClick(android.view.View v) {
+
+        }
+    }
+}
